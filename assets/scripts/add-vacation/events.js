b0e323e550f760ca28b4f491f45f8e85ef834fc2
dianamcnulty <dianamcnulty@gmail.com>
2017-11-25 11:01:54 -0500

    Add story feature functions
    -added handlebar for adding stories and photos
    -removed the photo/story fields from the add vacation handlebar
    (changing to 2 step process)
    -added story ajax calls
    -added event handlers and behavior for add story, skip step, and done
    buttons
    (both done and skip return to map page for now, later will go to story
    page.)

--- assets/scripts/add-vacation/events.js
+++ assets/scripts/add-vacation/events.js
@@ -1,7 +1,9 @@
 'use strict'
 const getFormFields = require(`../../../lib/get-form-fields`)
 const vacationAPI = require('../API/vacation-api')
+const storyAPI = require('../API/story-api')
 const mapEvents = require('../map-view/events')
+const storyPhotoTemplate = require('../templates/add-story-photo.handlebars')
 
 const onCreateVacation = function (event) {
   event.preventDefault()
@@ -9,14 +11,31 @@ const onCreateVacation = function (event) {
   vacationAPI.createVacation(data)
     .then((response) => {
       console.log('Vacation data is', response)
-      mapEvents.backToMap()
+      $('#content-container').html(storyPhotoTemplate(response))
     })
     .catch((response) => console.log('fail!'))
+}
+const onSaveStory = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  console.log('story data is', data)
+  storyAPI.createStory(data)
+    .then(response => {
+      const title = response.story.title
+      console.log('title is', title)
+      $('#story-container').append('<label>' + title + '</label><br>')
+      $('#story-title-field').val('')
+      $('#story-content-field').val('')
+    })
+    .catch(console.error)
 }
 const newVacationHandlers = function () {
   $('.add-input').val('')
   $(document).on('submit', '#add-vacation-form', onCreateVacation)
   $(document).on('click', '#cancel-new-vacation', mapEvents.backToMap)
+  $(document).on('submit', '#add-story', onSaveStory)
+  $(document).on('click', '#done', mapEvents.backToMap)
+  $(document).on('skip', '#done', mapEvents.backToMap)
 }
 module.exports = {
   newVacationHandlers
