661c9707dd096fee69c3d7be8bf5f0a1204e8f5c
dianamcnulty <dianamcnulty@gmail.com>
2017-11-25 12:16:05 -0500

    story shows in feature section
    when link is clicked story 'show' api call is made and story is
    displayed in feature section.
    add a story button on the content page is functional and takes the user
    to add photos/stories page

--- assets/scripts/content-view/events.js
+++ assets/scripts/content-view/events.js
@@ -1,6 +1,8 @@
 'use strict'
 const vacationAPI = require('../API/vacation-api')
+const storyAPI = require('../API/story-api')
 const mapEvents = require('../map-view/events')
+const storyPhotoTemplate = require('../templates/add-story-photo.handlebars')
 
 const deleteVacation = function (event) {
   const vacationId = event.target.dataset.id
@@ -16,10 +18,24 @@ const deleteVacation = function (event) {
 const updateVacation = function (event) {
   console.log('clicked the update button. vacation id is', event.target.dataset.id)
 }
-
+const showStory = function (event) {
+  const storyId = event.target.dataset.id
+  storyAPI.getOneStory(storyId)
+    .then(response => {
+      $('#highlighted-content').html('<h3>' + response.story.title + '</h3><br><p>' + response.story.content + '</p>')
+    })
+    .catch(console.error)
+}
+const goToStoryPhotos = function (event) {
+  const vacation = {vacation: {id: event.target.dataset.id}}
+  console.log('vacation is ', vacation)
+  $('#content-container').html(storyPhotoTemplate(vacation))
+}
 const contentViewHandlers = function () {
   $(document).on('click', '#delete-vacation-btn', deleteVacation)
   $(document).on('click', '#update-vacation-btn', updateVacation)
+  $(document).on('click', '.story-link', showStory)
+  $(document).on('click', '#add-story-photo', goToStoryPhotos)
 }
 
 module.exports = {
