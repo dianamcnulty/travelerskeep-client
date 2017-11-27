d268353a651c12311a408d0771c1b2e2e3a44038
dianamcnulty <dianamcnulty@gmail.com>
2017-11-27 15:40:58 -0500

    users now return to vacation view when they submit updates to stories or click cancel next: include delete functionality

--- assets/scripts/updates/events.js
+++ assets/scripts/updates/events.js
@@ -4,28 +4,44 @@ const getFormFields = require(`../../../lib/get-form-fields`)
 const contentTemplate = require('../templates/content.handlebars')
 const storyAPI = require('../API/story-api')
 
+const sendToTrip = function (id) {
+  vacationAPI.getOneVacation(id)
+    .then(vacation => {
+      console.log('vacation is', vacation)
+      $('#map-view').hide()
+      $('#content-container').html(contentTemplate(vacation))
+    })
+    .catch(console.error)
+}
 const onUpdateTrip = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   console.log('updateData is', data)
   const id = data.vacation.id
   vacationAPI.updateVacation(data)
-    .then((response) => vacationAPI.getOneVacation(id))
-    .then(vacation => {
-      console.log('vacation is', vacation)
-      $('#map-view').hide()
-      $('#content-container').html(contentTemplate(vacation))
-    })
+    .then((response) => sendToTrip(id))
+    .catch(console.error)
 }
 const onEditStory = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   console.log('clicked submit edit story data is', data)
   storyAPI.updateStory(data)
+    .then(response => {
+      const id = response.story.vacation.id
+      sendToTrip(id)
+    })
+    .catch(console.error)
+}
+const cancelUpdate = function (event) {
+  const vacationId = event.target.dataset.id
+  console.log('vacationId is', vacationId)
+  sendToTrip(vacationId)
 }
 const updateViewHandlers = function () {
   $(document).on('submit', '#update-vacation-form', onUpdateTrip)
   $(document).on('submit', '#edit-story', onEditStory)
+  $(document).on('click', '#cancel-update-story', cancelUpdate)
 }
 
 module.exports = {
