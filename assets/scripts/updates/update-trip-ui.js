4acccccef851f8a53230e8fad408ca69360048e8
dianamcnulty <dianamcnulty@gmail.com>
2017-11-30 15:47:55 -0500

    make countries with mult words and '.' work in the edit dropdown

--- assets/scripts/updates/update-trip-ui.js
+++ assets/scripts/updates/update-trip-ui.js
@@ -3,16 +3,26 @@ const editTripTemplate = require('../templates/updateVacation.handlebars')
 const countryAPI = require('../API/country-api')
 // similar functionality to add vacation, but uses vacation data to populate the fields.
 const showEditVacation = function (vacation) {
+  console.log('vacation is', vacation)
   const states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
   countryAPI.getAllCountries()
     .then((countries) => {
       $('#map-view').hide()
       $('#section-alerts').html('')
+      countries.countries.forEach((country) => {
+        // to use countryname for id's in template need to provied a version without spaces and punctuation
+        country.idValue = country.name.replace(/ /g, '').replace(/\./g, '')
+      })
       const places = {places: {nations: countries, states: states}}
+      console.log('places is', places)
       $('#content-container').html(editTripTemplate(places))
+      return countries
+    })
+    .then(() => {
       const trip = vacation.vacation
-      // this selects the correct country, state, and month in the dropdowns on the edit page
-      $('#' + trip.country).attr('selected', 'selected')
+      const vacationCountry = trip.country.replace(/ /g, '').replace(/\./g, '')
+      console.log('vacationCountry is', vacationCountry)
+      $('#' + vacationCountry).attr('selected', 'selected')
       if (trip.state) { $('#' + trip.state).attr('selected', 'selected') }
       if (trip.month) { $('#' + trip.month).attr('selected', 'selected') }
       $('#year').val(trip.year)
